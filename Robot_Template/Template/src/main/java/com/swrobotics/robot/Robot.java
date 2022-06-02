f1cbcc0d4bd052281ca66c7692c3aaf33d59241d
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2022-06-02 10:20:11 -0500

    Created an Input class for motor testing

--- Robot_Template/Template/src/main/java/com/swrobotics/robot/Robot.java
+++ Robot_Template/Template/src/main/java/com/swrobotics/robot/Robot.java
@@ -1,12 +1,21 @@
 package com.swrobotics.robot;
 
+import com.swrobotics.robot.input.Input;
+import com.swrobotics.robot.tests.MotorTest;
+
 import edu.wpi.first.hal.HAL;
 import edu.wpi.first.wpilibj.RobotBase;
 
 public final class Robot extends RobotBase {
+  private MotorTest test = new MotorTest();
+  private Input input = new Input();
+
   @Override
   public void startCompetition() {
     HAL.observeUserProgramStarting();
+
+    // TODO: Is this periodic?
+
   }
 
   @Override
