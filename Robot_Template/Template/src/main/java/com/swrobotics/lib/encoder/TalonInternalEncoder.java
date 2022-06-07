0c97c68c6790fee6e3295e3e888826319b9d9e6c
rmheuer <63077980+rmheuer@users.noreply.github.com>
2022-06-07 09:29:55 -0500

    Change ticks per degree to ticks per rotation to make the number more clear

--- Robot_Template/Template/src/main/java/com/swrobotics/lib/encoder/TalonInternalEncoder.java
+++ Robot_Template/Template/src/main/java/com/swrobotics/lib/encoder/TalonInternalEncoder.java
@@ -6,27 +6,27 @@ import com.swrobotics.lib.math.Angle;
 public class TalonInternalEncoder extends Encoder {
 
     private final BaseTalon talon;
-    private final double ticksPerDegree;
+    private final double ticksPerRotation;
 
     /**
      * Convert an internal encoder on a Falcon500 or a connected encoder on an SRX into an Enocder type
      * @param talon The motor controller with the sensor attached
-     * @param ticksPerDegree The number of sensor ticks that converts into 1 degree. The direction of this degree
+     * @param ticksPerRotation The number of sensor ticks that converts into 1 rotation. The direction of this rotatoin
      * determines that direction of clockwise on the sensor.
      */
-    public TalonInternalEncoder(BaseTalon talon, double ticksPerDegree) {
+    public TalonInternalEncoder(BaseTalon talon, double ticksPerRotation) {
         this.talon = talon;
-        this.ticksPerDegree = ticksPerDegree;
+        this.ticksPerRotation = ticksPerRotation;
     }
 
     @Override
     public Angle getRawAngle() {
-        return Angle.ccwDeg(talon.getSelectedSensorPosition() / ticksPerDegree);
+        return Angle.ccwDeg(talon.getSelectedSensorPosition() / (ticksPerRotation / 360));
     }
 
     @Override
     // TODO: fix this one too
     public Angle getVelocity() {
-        return Angle.ccwDeg(talon.getSelectedSensorVelocity() * ticksPerDegree * 10 /* Convert to seconds */);
+        return Angle.ccwDeg(talon.getSelectedSensorVelocity() * ticksPerRotation * 10 /* Convert to seconds TODO*/);
     }
 }
