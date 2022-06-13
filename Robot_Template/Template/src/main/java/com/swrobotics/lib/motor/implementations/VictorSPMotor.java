4f84355c8c4591e3fe43298b54ac7c95dedfcae8
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2022-06-13 10:42:40 -0500

    Created a VictorSP to control the SP's on the swerve prototype.

--- Robot_Template/Template/src/main/java/com/swrobotics/lib/motor/implementations/VictorSPMotor.java
+++ Robot_Template/Template/src/main/java/com/swrobotics/lib/motor/implementations/VictorSPMotor.java
@@ -1,55 +1,20 @@
 package com.swrobotics.lib.motor.implementations;
 
-import com.swrobotics.lib.encoder.Encoder;
-import com.swrobotics.lib.math.Angle;
 import com.swrobotics.lib.motor.Motor;
 
-import edu.wpi.first.math.controller.BangBangController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.controller.SimpleMotorFeedforward;
 import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
 
 public class VictorSPMotor extends Motor {
 
     private final VictorSP victor;
 
-    // private final ProfiledPIDController pid;
-    // private SimpleMotorFeedforward feed;
-    // private final BangBangController bang;
-
-    private boolean flywheel;
-
-    public VictorSPMotor(VictorSP victor) {
-        this.victor = victor;
-    }
-
-    // public VictorSPMotor(VictorSP victor, Encoder externalEncoder) {
-    //     super(externalEncoder);
-    //     this.victor = victor;
-    // }
-
-    // public VictorSPMotor(VictorSP victor, Encoder externalEncoder, )
-
-    @Override
-    public Encoder getInternalEncoder(double ticksPerRotation) {
-        throw new RuntimeException("Victor SP does not have encoder support");
+    public VictorSPMotor(int channel) {
+        victor = new VictorSP(channel);
     }
 
     @Override
-    protected void percent(double percent) {
+    protected void setPercent(double percent) {
         victor.set(percent);
-    }
-
-    @Override
-    protected void velocity(Angle current, Angle target) {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    protected void angle(Angle current, Angle target) {
-        // TODO Auto-generated method stub
-        
     }
     
 }
