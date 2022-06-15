cafb4378218742205c1c516279d0e442b7c99293
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2022-06-15 09:38:28 -0500

    Imported constants because it's confusing

--- Robot_Template/Template/src/main/java/com/swrobotics/robot/subsystem/Drive.java
+++ Robot_Template/Template/src/main/java/com/swrobotics/robot/subsystem/Drive.java
@@ -1,6 +1,7 @@
 package com.swrobotics.robot.subsystem;
 
 import com.swrobotics.lib.routine.Routine;
+import static com.swrobotics.robot.Constants.*; // This lets us just type the variable
 
 public class Drive extends Routine { // 'extends Routine' lets the subsystem do something every robot cycle (all the time)
 
