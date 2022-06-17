2b803018a44c8bef68270d7f477768d183c09bed
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2022-06-17 09:25:00 -0500

    Added yolov5 data

--- Odi_Vision/detector.py
+++ Odi_Vision/detector.py
@@ -4,5 +4,18 @@ import numpy as np
 import cv2
 
 # Import YOLO v5
-yolo = torch.hub.load('ultralytics/yolov5', 'yolov5m') # This uses the "medium" YOLO v5 option
+yolo = torch.hub.load('ultralytics/yolov5', 'custom', path='yolov5/runs/train/exp7/weights/last.pt', force_reload=True) # This uses the "medium" YOLO v5 option
 
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret, frame = cap.read()
+
+    results = yolo(frame)
+
+    cv2.imshow("YOLO", np.squeeze(results.render()))
+
+    if cv2.waitKeyEx(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows()
