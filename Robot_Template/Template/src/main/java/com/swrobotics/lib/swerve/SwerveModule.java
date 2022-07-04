d407948a4c5736c43db3746b0e6dc0edf92164e9
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2022-07-03 23:17:06 -0500

    Creaated a very basic swerve module that will not work.

--- Robot_Template/Template/src/main/java/com/swrobotics/lib/swerve/SwerveModule.java
+++ Robot_Template/Template/src/main/java/com/swrobotics/lib/swerve/SwerveModule.java
@@ -1,44 +1,49 @@
 package com.swrobotics.lib.swerve;
 
-import com.ctre.phoenix.motorcontrol.can.BaseTalon;
-import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.swrobotics.lib.encoder.AbsoluteEncoder;
-import com.swrobotics.lib.encoder.CANCoderImplementation;
 import com.swrobotics.lib.encoder.Encoder;
-import com.swrobotics.lib.encoder.TalonInternalEncoder;
-import com.swrobotics.lib.motor.implementations.TalonMotor;
+import com.swrobotics.lib.math.Angle;
+import com.swrobotics.lib.motor.Motor;
 
-/**
- * A single swerve module that controls both speed and azimuth (steer).
- */
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
 public class SwerveModule {
 
-    private final TalonMotor drive;
-    private final TalonMotor steer;
-    
+    private final Motor driveMotor;
+    private final Motor steerMotor;
+
     private final Encoder driveEncoder;
     private final AbsoluteEncoder steerEncoder;
 
+    // TODO: Tolerence and odometry
+    
     /**
-     * Create a swerve module with already-configured motors and encoders
-     * @param drive_toWrap
-     * @param steer_toWrap
-     * @param driveEncoder
-     * @param steerEncoder
+     * A single swerve module capable of both steering and driving.
+     * @param driveMotor A motor to drive the module, already configured with an encoder.
+     * @param steerMotor A motor to steer the module.
+     * @param steerEncoder An absolute encoder to read the angle of the module.
      */
-    public SwerveModule(BaseTalon drive_toWrap, BaseTalon steer_toWrap, Encoder driveEncoder, AbsoluteEncoder steerEncoder) {
+    public SwerveModule(Motor driveMotor, Motor steerMotor, AbsoluteEncoder steerEncoder) {
+        this.driveMotor = driveMotor;
+        this.driveEncoder = driveMotor.getEncoder();
 
-       // Setup encoders
-        this.driveEncoder = driveEncoder;
+        this.steerMotor = steerMotor;
         this.steerEncoder = steerEncoder;
+        steerMotor.assignEncoder(steerEncoder);
+    }
 
-        // Create motors and assign encoders
-        drive = new TalonMotor(drive_toWrap);
-        drive.assignEncoder(driveEncoder);
+    public void setState(SwerveModuleState desiredState) {
+        // Set steer angle
+        steerMotor.angle(Angle.cwDeg(desiredState.angle.getDegrees())); // Check direction
 
-        steer = new TalonMotor(steer_toWrap);
-        steer.assignEncoder(steerEncoder);
+        // Set drive speed
+        driveMotor.velocity(Angle.cwDeg(desiredState.speedMetersPerSecond * 360 )); // TODO: Velocity to motor conversion
+    }
 
+    public SwerveModuleState getState() {
+        double velocity = driveEncoder.getVelocity().getCWDeg(); // Check direction, TODO: Motor to velocity conversion
+        Angle angle = steerEncoder.getAngle();
 
+        return new SwerveModuleState(velocity, angle.toRotation2dCW());
     }
 }
