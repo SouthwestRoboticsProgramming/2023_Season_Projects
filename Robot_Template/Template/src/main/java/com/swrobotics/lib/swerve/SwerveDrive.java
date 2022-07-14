abef3abef0fa9d7d30cabb010de1fd1a33aa8d57
Mason Vogt <91571536+mvog2501@users.noreply.github.com>
2022-07-13 21:54:27 -0500

    Created a SwerveDrive.java with minor changes from Ryan's.

---
+++ Robot_Template/Template/src/main/java/com/swrobotics/lib/swerve/SwerveDrive.java
@@ -0,0 +1,67 @@
+package com.swrobotics.lib.swerve;
+
+import com.swrobotics.lib.gyro.Gyroscope;
+import com.swrobotics.lib.math.Angle;
+import com.swrobotics.lib.math.Vec2d;
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+
+/** A class to manage all of the swerve modules in a swerve drive. */
+public class SwerveDrive {
+    private final Gyroscope gyro;
+    private final SwerveModule[] modules;
+
+    private final double maxWheelVelocity;
+    private final SwerveDriveKinematics kinematics;
+
+    private Vec2d centerOfRotation;
+
+    public SwerveDrive(Gyroscope gyro, double maxWheelVelocity, SwerveModule... modules) {
+        this.gyro = gyro;
+        this.modules = modules;
+
+        // Extract positions of modules
+        Translation2d[] positions = new Translation2d[modules.length];
+        for (int i = 0; i< modules.length; i++) {
+            positions[i] = modules[i].getPosition().toTranslation2d();
+        }
+
+        kinematics = new SwerveDriveKinematics(positions);
+        this.maxWheelVelocity = maxWheelVelocity;
+
+        centerOfRotation = new Vec2d(0, 0);
+    }
+
+    /**
+     * Set a custom center of rotation away from the center of the robot
+     * @param newCenterOfRotation Center of rotatoin relative to the center of the robot
+     */
+    public void setCenterOfRotation(Vec2d newCenterOfRotation) {
+        centerOfRotation = newCenterOfRotation;
+    }
+
+    /**
+     * Set the desired motion of the swerve drive
+     * @param translation Desired veclocity vector in meters per second
+     * @param rotationsPerSecond Desired rotational velocity in rotation per second
+     */
+    public void setMotion(Vec2d translation, Angle rotationsPerSecond) {
+        ChassisSpeeds speeds = ChassisSpeeds.fromFieldRelativeSpeeds(
+            translation.y, 
+            -translation.x, // Convert from our coordinates to WPILIb
+            rotationsPerSecond.getCCWRad(), 
+            gyro.getAngle().toRotation2dCCW()
+            );
+
+        SwerveModuleState[] states = kinematics.toSwerveModuleStates(speeds, centerOfRotation.toTranslation2d());
+        SwerveDriveKinematics.desaturateWheelSpeeds(states, maxWheelVelocity);
+
+        // Update modules
+        for (int i = 0; i < states.length; i++) {
+            modules[i].setState(states[i]);
+        }
+    }
+}
--- Robot_Template/Template/src/main/java/com/swrobotics/lib/swerve/SwerveModule.java
+++ Robot_Template/Template/src/main/java/com/swrobotics/lib/swerve/SwerveModule.java
@@ -3,6 +3,7 @@ package com.swrobotics.lib.swerve;
 import com.swrobotics.lib.encoder.AbsoluteEncoder;
 import com.swrobotics.lib.encoder.Encoder;
 import com.swrobotics.lib.math.Angle;
+import com.swrobotics.lib.math.Vec2d;
 import com.swrobotics.lib.motor.Motor;
 
 import edu.wpi.first.math.kinematics.SwerveModuleState;
@@ -15,6 +16,8 @@ public class SwerveModule {
     private final Encoder driveEncoder;
     private final AbsoluteEncoder steerEncoder;
 
+    private final Vec2d position;
+
     // TODO: Tolerence and odometry
     
     /**
@@ -22,17 +25,25 @@ public class SwerveModule {
      * @param driveMotor A motor to drive the module, already configured with an encoder.
      * @param steerMotor A motor to steer the module.
      * @param steerEncoder An absolute encoder to read the angle of the module.
+     * @param position The position of the module in meters, relative to the center of rotation of the robot.
      */
-    public SwerveModule(Motor driveMotor, Motor steerMotor, AbsoluteEncoder steerEncoder) {
+    public SwerveModule(Motor driveMotor, Motor steerMotor, AbsoluteEncoder steerEncoder, Vec2d position) {
         this.driveMotor = driveMotor;
         this.driveEncoder = driveMotor.getEncoder();
 
         this.steerMotor = steerMotor;
         this.steerEncoder = steerEncoder;
         steerMotor.assignEncoder(steerEncoder);
+
+        this.position = position;
+    }
+
+    public Vec2d getPosition() {
+        return position;
     }
 
     public void setState(SwerveModuleState desiredState) {
+        SwerveModuleState.optimize(desiredState, steerEncoder.getAngle().toRotation2dCW()); // Check angle measurement
         // Set steer angle
         steerMotor.angle(Angle.cwDeg(desiredState.angle.getDegrees())); // Check direction
 
