6b83c97b9e2e7700323fc4703dd9260ac5021c49
rmheuer <91571536+mvog2501@users.noreply.github.com>
2022-07-19 15:41:53 -0500

    Minor tuning changes and cleanup

--- Robot_Template/Template/src/main/java/com/swrobotics/robot/Constants.java
+++ Robot_Template/Template/src/main/java/com/swrobotics/robot/Constants.java
@@ -5,24 +5,22 @@ public final class Constants {
     public static final double DRIVE_KP = 0.0001;
     public static final double DRIVE_KI = 0;
     public static final double DRIVE_KD = 0;
-    public static final double TURN_KP = 0.004;
+    public static final double TURN_KP = 0.007;
     public static final double TURN_KI = 0.0001;
     public static final double TURN_KD = 0.0;
 
-    public static final double MAX_WHEEL_VELOCITY = 4.0;
+    public static final double MAX_WHEEL_VELOCITY = 4.11;
     public static final double GEAR_RATIO = 1 / 8.14;
     public static final double WHEEL_RADIUS = 0.05;
     public static final double WHEEL_SPACING = 29.3;
 
-    public static final double MAX_DRIVE_VELOCITY = 4.11;
 
+    public static final double MAX_DRIVE_ROTATION = 0.5 * Math.PI;
+    public static final double MAX_DRIVE_SPEED = 0.02;
 
-    public static final double MAX_DRIVE_ROTATION = 2 * Math.PI;
-    public static final double MAX_DRIVE_SPEED = 0.20;
+    public static final double INPUT_ACCELERATION = 2.0; // Units per second
 
-    public static final double INPUT_ACCELERATION = 2.0; // per second
-
-    private Constants() { // Don't do ' new Constants '
-        throw new AssertionError(); // Ignore this
+    private Constants() {
+        throw new AssertionError();
     }
 }
