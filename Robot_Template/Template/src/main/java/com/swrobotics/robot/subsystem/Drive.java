303f37381fc4db1301fcda1ea16f90693b572337
rmheuer <91571536+mvog2501@users.noreply.github.com>
2022-07-19 15:41:29 -0500

    Renamed modules to match stickers on robot

--- Robot_Template/Template/src/main/java/com/swrobotics/robot/subsystem/Drive.java
+++ Robot_Template/Template/src/main/java/com/swrobotics/robot/subsystem/Drive.java
@@ -24,16 +24,16 @@ public class Drive extends Routine {
         
         double centerDistance = WHEEL_SPACING / 2;
 
-        SwerveModule w1 = SwerveModuleMaker.buildModule(5, 11, 10, Angle.cwDeg(178.857 + 180), new Vec2d(-centerDistance, -centerDistance));
-        SwerveModule w2 = SwerveModuleMaker.buildModule(3, 12, 8, Angle.cwDeg(78.923 + 180), new Vec2d(-centerDistance, centerDistance));
-        SwerveModule w3 = SwerveModuleMaker.buildModule(4, 14, 9, Angle.cwDeg(3.164), new Vec2d(centerDistance, centerDistance));
-        SwerveModule w4 = SwerveModuleMaker.buildModule(1, 13, 6, Angle.cwDeg(132.363), new Vec2d(centerDistance, -centerDistance));
+        SwerveModule w0 = SwerveModuleMaker.buildModule(5, 11, 10, Angle.cwDeg(206.016), new Vec2d(-centerDistance, -centerDistance));
+        SwerveModule w1 = SwerveModuleMaker.buildModule(3, 12, 8, Angle.cwDeg(78.574 + 180), new Vec2d(-centerDistance, centerDistance));
+        SwerveModule w2 = SwerveModuleMaker.buildModule(1, 13, 6, Angle.cwDeg(310.253 + 180), new Vec2d(centerDistance, -centerDistance));
+        SwerveModule w3 = SwerveModuleMaker.buildModule(4, 14, 9, Angle.cwDeg(3.9550 + 180), new Vec2d(centerDistance, centerDistance));
         
         drive = new SwerveDrive(gyro, MAX_WHEEL_VELOCITY,
+        w0,
         w1,
         w2,
-        w3,
-        w4
+        w3
         );
     }
 
