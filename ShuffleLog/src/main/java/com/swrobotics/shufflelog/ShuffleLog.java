37c9713cdc0400ebb3343556f6ba977a111c22b6
rmheuer <63077980+rmheuer@users.noreply.github.com>
2022-07-22 10:28:17 -0500

    Integrate libp5x into ShuffleLog

--- ShuffleLog/src/main/java/com/swrobotics/shufflelog/ShuffleLog.java
+++ ShuffleLog/src/main/java/com/swrobotics/shufflelog/ShuffleLog.java
@@ -9,36 +9,37 @@ import com.swrobotics.shufflelog.tool.ShuffleLogProfilerTool;
 import com.swrobotics.shufflelog.tool.Tool;
 import imgui.ImGui;
 import imgui.ImGuiIO;
-import imgui.app.Application;
-import imgui.app.Configuration;
-import imgui.extension.implot.ImPlot;
-import imgui.extension.implot.ImPlotContext;
+import imgui.flag.ImGuiMouseButton;
+import imgui.gl3.ImGuiImplGl3;
+import processing.core.PApplet;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public final class ShuffleLog extends Application {
+public final class ShuffleLog extends PApplet {
     private final List<Tool> tools = new ArrayList<>();
     private final ExecutorService threadPool = Executors.newFixedThreadPool(4);
-    private ImPlotContext plotCtx;
+    private ProcessingImGuiBackend gui;
     private MessengerClient msg;
 
     @Override
-    protected void configure(Configuration config) {
-        config.setTitle("ShuffleLog");
+    public void settings() {
+        size(1280, 720, P2D);
     }
 
     @Override
-    protected void initImGui(Configuration config) {
-        super.initImGui(config);
-        plotCtx = ImPlot.createContext();
+    public void setup() {
+        surface.setResizable(true);
 
+        ImGui.createContext();
         ImGuiIO io = ImGui.getIO();
         io.setIniFilename("layout.ini");
-
         Styles.applyDark();
+
+        gui = new ProcessingImGuiBackend();
+        gui.init();
 
         tools.add(new MenuBarTool());
         tools.add(new MessengerTool(this));
@@ -47,8 +48,17 @@ public final class ShuffleLog extends Application {
     }
 
     @Override
-    public void process() {
+    public void draw() {
         Profiler.beginMeasurements("Root");
+
+        Profiler.push("Begin GUI frame");
+        ImGuiIO io = ImGui.getIO();
+        io.setDisplaySize(width, height);
+        io.setDeltaTime(1 / 60.0f);
+        io.setMousePos(mouseX, mouseY);
+        io.setMouseDown(ImGuiMouseButton.Left, mousePressed);
+        ImGui.newFrame();
+        Profiler.pop();
 
         Profiler.push("Read Messages");
         msg.readMessages();
@@ -60,17 +70,17 @@ public final class ShuffleLog extends Application {
             Profiler.pop();
         }
 
+        Profiler.push("Render GUI");
+        Profiler.push("Flush");
+        flush();
+        Profiler.pop();
+        Profiler.push("Render draw data");
+        ImGui.render();
+        gui.renderDrawData(ImGui.getDrawData());
+        Profiler.pop();
+        Profiler.pop();
+
         Profiler.endMeasurements();
-    }
-
-    @Override
-    protected void disposeImGui() {
-        threadPool.shutdown();
-
-        ImPlot.destroyContext(plotCtx);
-        super.disposeImGui();
-
-        msg.disconnect();
     }
 
     public MessengerClient getMsg() {
@@ -86,6 +96,6 @@ public final class ShuffleLog extends Application {
     }
 
     public static void main(String[] args) {
-        launch(new ShuffleLog());
+        PApplet.main(ShuffleLog.class);
     }
 }
