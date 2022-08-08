be77d512fae673b99e84d3b11e6ec3457bd2a2b8
rmheuer <63077980+rmheuer@users.noreply.github.com>
2022-08-08 14:13:41 -0500

    Pathfinding: Add grid union to combine grids

---
+++ Pathfinding/Pathfinding-Lib/src/main/java/com/swrobotics/pathfinding/lib/grid/GridUnion.java
@@ -0,0 +1,35 @@
+package com.swrobotics.pathfinding.lib.grid;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public final class GridUnion extends Grid {
+    private final Set<Grid> children;
+
+    public GridUnion(int width, int height) {
+        super(width, height);
+        children = new HashSet<>();
+    }
+
+    // Cell is passable if all children's cells are passable
+    @Override
+    public boolean canCellPass(int x, int y) {
+        for (Grid grid : children) {
+            if (!grid.canCellPass(x, y)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void addGrid(Grid grid) {
+        if (grid.getCellWidth() != getCellWidth() || grid.getCellHeight() != getCellHeight())
+            throw new IllegalArgumentException("Grid size is not compatible");
+
+        children.add(grid);
+    }
+
+    public void removeGrid(Grid grid) {
+        children.remove(grid);
+    }
+}
