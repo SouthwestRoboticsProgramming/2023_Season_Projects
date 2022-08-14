250cf76d2ccfa8892d5dda2b8efca0ee79eea234
rmheuer <91571536+mvog2501@users.noreply.github.com>
2022-08-14 09:32:22 -0500

    Added CommandSequence and fixed Command docs

--- Robot_Template/Template/src/main/java/com/team2129/lib/schedule/Command.java
+++ Robot_Template/Template/src/main/java/com/team2129/lib/schedule/Command.java
@@ -4,12 +4,16 @@ import com.team2129.lib.time.Duration;
 import com.team2129.lib.time.TimeUnit;
 import com.team2129.lib.wpilib.AbstractRobot;
 
+// TODO: DOCS!!!!!!!!
 public interface Command {
     Duration DEFAULT_INTERVAL = new Duration(1 / AbstractRobot.get().getPeriodicPerSecond(), TimeUnit.SECONDS);
 
     default void init() {}
 
-    // Returns true when complete
+    /**
+     * This function runs every periodic until it returns true.
+     * @return Returns true when command is compete.
+     */
     boolean run();
 
     default void end(boolean wasCancelled) {}
