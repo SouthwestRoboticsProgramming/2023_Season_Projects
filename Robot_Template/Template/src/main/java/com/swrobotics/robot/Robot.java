bd909dab06c87fa9cae33effdb2294cb1777be27
rmheuer <63077980+rmheuer@users.noreply.github.com>
2022-08-15 15:44:59 -0500

    Template: Connect to RPi Messenger when on real robot

--- Robot_Template/Template/src/main/java/com/swrobotics/robot/Robot.java
+++ Robot_Template/Template/src/main/java/com/swrobotics/robot/Robot.java
@@ -1,8 +1,8 @@
 package com.swrobotics.robot;
 
 import com.swrobotics.robot.control.Input;
-import com.swrobotics.robot.subsystem.Drive;
 import com.swrobotics.robot.subsystem.Localization;
+import com.swrobotics.robot.subsystem.drive.Drive;
 import com.swrobotics.robot.subsystem.thrower.Thrower;
 import com.team2129.lib.net.NTDouble;
 import com.team2129.lib.schedule.Scheduler;
@@ -10,6 +10,7 @@ import com.team2129.lib.wpilib.AbstractRobot;
 
 public final class Robot extends AbstractRobot {
     private static final double PERIODIC_PER_SECOND = 50;
+    private static final String RASPBERRY_PI_IP = "10.21.29.3";
 
     public Robot() {
 	    super(PERIODIC_PER_SECOND);
@@ -17,7 +18,13 @@ public final class Robot extends AbstractRobot {
 	    // Provide connection parameters and enable Messenger
         // Important: This does not prevent the code from working without Messenger!
         //            It only tells the Messenger client to try to connect.
-	    initMessenger("localhost", 5805, "Robot");
+	    if (isSimulation()) {
+            // Connect to local Messenger server
+            initMessenger("localhost", 5805, "Robot");
+        } else {
+            // Connect to Raspberry Pi
+            initMessenger(RASPBERRY_PI_IP, 5805, "Robot");
+        }
     }
     
     @Override
