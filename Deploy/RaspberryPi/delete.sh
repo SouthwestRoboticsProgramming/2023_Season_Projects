170954b89ec0c66c8e6d1f812f8e4229a223846c
rmheuer <63077980+rmheuer@users.noreply.github.com>
2022-08-15 16:43:52 -0500

    Deploy/RaspberryPi: Stop services before disabling them

--- Deploy/RaspberryPi/delete.sh
+++ Deploy/RaspberryPi/delete.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 # This file undoes all system changes done by init.sh
 
+echo "Stopping services"
+sudo systemctl stop taskmanager
+sudo systemctl stop messenger
+
+echo "Disabling services"
 sudo systemctl disable messenger
 sudo systemctl disable taskmanager
 
