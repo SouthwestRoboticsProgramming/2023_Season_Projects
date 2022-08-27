006c3f79483216c26ee1d2d970d9febc407190b2
rmheuer <91571536+mvog2501@users.noreply.github.com>
2022-08-26 22:32:36 -0500

    [Template] Allow for legacy TalonFXMotor creation.
    To allow for a custom configured TalonFX, I included the way the legacy code worked in that it would just wrap a TalonFX instead of creating a new one.

--- Robot_Template/Template/src/main/java/com/team2129/lib/motor/ctre/TalonFXMotor.java
+++ Robot_Template/Template/src/main/java/com/team2129/lib/motor/ctre/TalonFXMotor.java
@@ -4,7 +4,9 @@ import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.SupplyCurrentLimitConfiguration;
 import com.ctre.phoenix.motorcontrol.can.TalonFX;
 import com.ctre.phoenix.motorcontrol.can.TalonFXConfiguration;
+import com.ctre.phoenix.sensors.SensorInitializationStrategy;
 import com.team2129.lib.schedule.Subsystem;
+import com.team2129.lib.utils.TalonErrorChecker;
 
 /**
  * Represents a Talon FX motor.
@@ -35,19 +37,31 @@ public final class TalonFXMotor extends TalonMotor {
         super(parent, createConfiguredFX(canID, canBus), ENCODER_TICKS_PER_ROTATION);
     }
 
+    /**
+     * Creates a new instance that belongs to the specified {@code Subsystem}.
+     * This constructor allows you to build a TalonFXMotor using a TalonFX that you have already configured with custom settings.
+     * 
+     * @param parent Parent subsystem
+     * @param configuredTalon TalonFX already configured to fit your needs.
+     */
+    public TalonFXMotor(Subsystem parent, TalonFX configuredTalon) {
+        super(parent, configuredTalon, ENCODER_TICKS_PER_ROTATION)
+    }
+
     private static TalonFX createConfiguredFX(int canID, String canBus) {
         TalonFXConfiguration config = new TalonFXConfiguration();
         config.primaryPID.selectedFeedbackSensor = FeedbackDevice.IntegratedSensor;
+        config.initializationStrategy = SensorInitializationStrategy.BootToZero;
         config.supplyCurrLimit = new SupplyCurrentLimitConfiguration(
             true, // Enabled
             35, // Continuous current limit
-            70, // Peak current limit
+            60, // Peak current limit
             0.1 // Peak current limit duration
         );
         
         TalonFX fx = new TalonFX(canID, canBus);
-        fx.configFactoryDefault();
-        fx.configAllSettings(config);
+        fx.configFactoryDefault(100);
+        TalonErrorChecker.checkError(fx.configAllSettings(config, 100), "Failed to configure enhanced default TalonFX");
 
         return fx;
     }
