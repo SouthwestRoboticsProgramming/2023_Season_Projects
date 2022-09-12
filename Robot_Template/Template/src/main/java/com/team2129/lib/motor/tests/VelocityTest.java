f390ae2d629784d0c7b79891fb339380f5d2899c
rmheuer <91571536+mvog2501@users.noreply.github.com>
2022-09-11 22:58:21 -0500

    [Template] Updated VelocityTest to actually test velocity.
    VelocityTest is now configured to actually test a motor by subtracting the actual angle from the target angle.

--- Robot_Template/Template/src/main/java/com/team2129/lib/motor/tests/VelocityTest.java
+++ Robot_Template/Template/src/main/java/com/team2129/lib/motor/tests/VelocityTest.java
@@ -1,22 +1,32 @@
 package com.team2129.lib.motor.tests;
 
+import com.team2129.lib.math.Angle;
+import com.team2129.lib.motor.Motor;
 import com.team2129.lib.schedule.TestCommand;
 
 public class VelocityTest extends TestCommand {
 
-    public VelocityTest(double timeoutSeconds) {
+    private final Motor motor;
+    private final Angle targetVelocity;
+    private final Angle tolerance;
+
+    public VelocityTest(Motor motor, double timeoutSeconds, Angle targetVelocity, Angle tolerance) {
         setTimeout(timeoutSeconds);
+        this.motor = motor;
+        this.targetVelocity = targetVelocity;
+        this.tolerance = tolerance;
     }
 
     @Override
     public boolean run() {
-        System.out.println(getTime());
+        motor.velocity(targetVelocity);
         return hasTimedOut();
     }
 
     @Override
     public boolean hasFailed() {
-        return hasTimedOut() && /*Not at setpoint*/ true;
+        Angle velocityDiff = targetVelocity.sub(motor.getEncoder().getVelocity());
+        return hasTimedOut() && velocityDiff.absoluteValue().lessOrEqualTo(tolerance);
     }
     
 }
