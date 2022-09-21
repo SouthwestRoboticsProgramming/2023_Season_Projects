bdb5b0f5a3fa13a16640065665660557971555dd
mvog2501 <mvog2501@mpsedu.org>
2022-09-21 09:34:18 -0500

    [Template] Add some limelight usage

--- Robot_Template/Template/src/main/java/com/swrobotics/robot/subsystem/Localization.java
+++ Robot_Template/Template/src/main/java/com/swrobotics/robot/subsystem/Localization.java
@@ -1,11 +1,16 @@
 package com.swrobotics.robot.subsystem;
 
+import com.swrobotics.robot.Robot;
 import com.team2129.lib.math.Angle;
 import com.team2129.lib.math.Vec2d;
+import com.team2129.lib.net.NTBoolean;
 import com.team2129.lib.schedule.Scheduler;
 import com.team2129.lib.schedule.Subsystem;
+import com.team2129.lib.wpilib.RobotState;
 
 public class Localization implements Subsystem {
+
+    private static final NTBoolean USE_LIMELIGHT = new NTBoolean("Limelight/Use for localization", true);
 
     private final Limelight limelight;
 
@@ -21,8 +26,11 @@ public class Localization implements Subsystem {
 
     @Override
     public void periodic() {
-        if (limelight.isAccurate()) {
-            // TODO
+
+        // Update position using limelight
+        // Only update if it is accurate, the robot is in teleop, and we actually want to use it.
+        if (limelight.isAccurate() && USE_LIMELIGHT.get() && Robot.get().getCurrentState() == RobotState.TELEOP) {
+            
         }
     }
 
