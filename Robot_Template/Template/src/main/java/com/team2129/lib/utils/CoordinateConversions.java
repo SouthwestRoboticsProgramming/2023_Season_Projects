105e5b2203e8aa431c8b62e9488962cea49b098e
rmheuer <91571536+mvog2501@users.noreply.github.com>
2022-09-24 22:41:34 -0500

    [Template] Add docs to Coordinate Conversions for clarity

--- Robot_Template/Template/src/main/java/com/team2129/lib/utils/CoordinateConversions.java
+++ Robot_Template/Template/src/main/java/com/team2129/lib/utils/CoordinateConversions.java
@@ -3,7 +3,23 @@ package com.team2129.lib.utils;
 import com.team2129.lib.math.Vec2d;
 import edu.wpi.first.math.geometry.Translation2d;
 
+/**
+ * This class converts between WPI's coordinate system, as defined on
+ * https://first.wpi.edu/wpilib/allwpilib/docs/release/java/edu/wpi/first/math/kinematics/ChassisSpeeds.html#fromFieldRelativeSpeeds(double,double,double,edu.wpi.first.math.geometry.Rotation2d)
+ * to our own coordinate system that we believe aligns more with mathematics practices.
+ * 
+ * 
+ * <pre>
+ * The WPI coordinate system goes as follows:
+ * "Positive x is away from your alliance wall"
+ * "Positive y is to your left when standing behind the alliance wall"
+ * 
+ * The Ultraviolet coordinate system goes as follows:
+ * Positive X is to the driver's right.
+ * Positive Y is away from the driver.
+ */
 public final class CoordinateConversions {
+
     public static Vec2d fromWPICoords(Translation2d tx) {
         return new Vec2d(-tx.getY(), tx.getX());
     }
