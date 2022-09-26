c25c76f0264794b21ccb866a9097fa471e560b3f
rmheuer <91571536+mvog2501@users.noreply.github.com>
2022-09-25 22:20:30 -0500

    [Template] Update wait command to start when the robot is enabled

--- Robot_Template/Template/src/main/java/com/team2129/lib/schedule/WaitCommand.java
+++ Robot_Template/Template/src/main/java/com/team2129/lib/schedule/WaitCommand.java
@@ -4,19 +4,29 @@ import com.team2129.lib.time.Duration;
 import com.team2129.lib.time.TimeUnit;
 import com.team2129.lib.time.Timestamp;
 
+// FIXME: Docs
 public final class WaitCommand implements Command {
-    private final Timestamp end;
+    private final Duration dur;
+    private Timestamp end;
+    private boolean initiated;
 
     public WaitCommand(double amt, TimeUnit unit) {
         this(new Duration(amt, unit));
     }
 
     public WaitCommand(Duration dur) {
-        end = Timestamp.now().after(dur);
+        initiated = false;
+        this.dur = dur;
     }
 
     @Override
     public boolean run() {
+        // Start the wait when the command starts
+        if (!initiated) {
+            end = Timestamp.now().after(dur);
+        }
+        System.out.println("Hi");
+        initiated = true;
         return Timestamp.now().isAtOrAfter(end);
     }
 }
