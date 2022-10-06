ada5034b1e2cf0647d667370d51f3323899b1201
mvog2501 <mvog2501@mpsedu.org>
2022-10-06 09:56:34 -0500

    [Template] Rotating arms now target a position

--- Robot_Template/Template/src/main/java/com/swrobotics/robot/subsystem/climber/RotatingArm.java
+++ Robot_Template/Template/src/main/java/com/swrobotics/robot/subsystem/climber/RotatingArm.java
@@ -1,13 +1,23 @@
 package com.swrobotics.robot.subsystem.climber;
 
+import java.util.function.Supplier;
+
 import com.team2129.lib.encoder.filters.JumpToZeroFilter;
 import com.team2129.lib.math.Angle;
-import com.team2129.lib.motor.rev.BrushedSparkMaxMotor;
+import com.team2129.lib.motor.calc.PIDCalculator;
+import com.team2129.lib.motor.rev.BrushlessSparkMaxMotor;
+import com.team2129.lib.net.NTDouble;
 import com.team2129.lib.schedule.Subsystem;
 
 import edu.wpi.first.math.MathUtil;
 
 public class RotatingArm implements Subsystem {
+
+    private static final NTDouble TEST_ROTS = new NTDouble("Test/Test/Rotating_Test_Pos", 0);
+
+    private static final NTDouble KP = new NTDouble("Climber/Rotating/kP", 0.01);
+    private static final NTDouble KI = new NTDouble("Climber/Rotating/kI", 0.0);
+    private static final NTDouble KD = new NTDouble("Climber/Rotating/kD", 0.0);
 
     private static final double ARM_LENGTH = 15.0;
     private static final double BASE_LENGTH = 6.76;
@@ -17,13 +27,20 @@ public class RotatingArm implements Subsystem {
     private static final int MIN_ANGLE_CWDEG = 40;
     private static final int MAX_ANGLE_CWDEG = 120;
 
-    private final BrushedSparkMaxMotor motor;
+    private final BrushlessSparkMaxMotor motor;
+
+    private final Supplier<Angle> currentAngle;
 
     private Angle targetAngle;
     
     public RotatingArm(int motorID) {
-        motor = new BrushedSparkMaxMotor(this, motorID);
+        motor = new BrushlessSparkMaxMotor(this, motorID);
         motor.setEncoderFilter(new JumpToZeroFilter());
+        motor.setPositionCalculator(new PIDCalculator(KP, KI, KD));
+
+        currentAngle = () -> getAngle();
+
+        setTargetAngle(Angle.cwDeg(90));
     }
 
     public void setTargetAngle(Angle angle) {
@@ -33,7 +50,8 @@ public class RotatingArm implements Subsystem {
 
     public Angle getAngle() {
         // Get angle from encoder
-        double encoderRots = motor.getEncoder().getAngle().getCWRot();
+        // double encoderRots = motor.getEncoder().getAngle().getCWRot();
+        double encoderRots = TEST_ROTS.get();
 
         // Calculate length of Igus Shaft between pivot points
         double currentIgusLength = encoderRots / ROTS_PER_INCH + CALIBRATED_LENGTH;
@@ -49,6 +67,9 @@ public class RotatingArm implements Subsystem {
         return Angle.cwRad(cwRad);
     }
 
-
+    @Override
+    public void periodic() {
+        motor.position(currentAngle, targetAngle);
+    }
     
 }
