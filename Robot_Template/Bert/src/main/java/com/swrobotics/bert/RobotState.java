2fe0c1dd88d756dbca2ca4d9b8ba4adc4089c226
2129 Student <ryanmichaelheuer2@gmail.com>
2022-10-19 22:10:25 -0500

    [Everything] Build with Gradle

--- Robot_Template/Bert/src/main/java/com/swrobotics/bert/RobotState.java
+++
@@ -1,8 +0,0 @@
-package com.swrobotics.bert;
-
-public enum RobotState {
-    DISABLED,
-    TELEOP,
-    AUTONOMOUS,
-    TEST
-}
--- Robot_Template/Bert/src/main/java/com/swrobotics/taskmanager/api/UploadResult.java
+++
@@ -1,7 +0,0 @@
-package com.swrobotics.taskmanager.api;
-
-public enum UploadResult {
-    SUCCESS,
-    ENCODE_FAILED,
-    DECODE_FAILED
-}
