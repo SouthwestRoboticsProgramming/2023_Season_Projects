faf6510dd868e36a02aae65b10ce663a5b5d65e6
2129 Student <ryanmichaelheuer2@gmail.com>
2022-10-25 11:02:20 -0500

    [ShuffleLog] Make camera controls smooth

--- ShuffleLog/src/main/java/com/swrobotics/shufflelog/tool/loc/LocalizationTool.java
+++ ShuffleLog/src/main/java/com/swrobotics/shufflelog/tool/loc/LocalizationTool.java
@@ -5,6 +5,7 @@ import com.swrobotics.shufflelog.math.Matrix4f;
 import com.swrobotics.shufflelog.math.Vector2f;
 import com.swrobotics.shufflelog.math.Vector3f;
 import com.swrobotics.shufflelog.tool.ViewportTool;
+import com.swrobotics.shufflelog.util.SmoothFloat;
 import imgui.ImGui;
 import imgui.ImGuiIO;
 import imgui.ImVec2;
@@ -25,11 +26,13 @@ import java.util.List;
 import static processing.core.PConstants.*;
 
 public final class LocalizationTool extends ViewportTool {
+    private static final float SMOOTH = 12;
+
     private final List<ReferenceTag> tags;
     private ReferenceTag selectedTag;
 
-    private final Vector2f cameraRot;
-    private final Vector3f cameraPos;
+    private final SmoothFloat cameraRotX, cameraRotY;
+    private final SmoothFloat cameraPosX, cameraPosY, cameraPosZ;
 
     private Matrix4f projection, view;
     private Matrix4f gizmoTarget;
@@ -43,8 +46,12 @@ public final class LocalizationTool extends ViewportTool {
         tags.add(new ReferenceTag("Tag 1", 1));
         tags.add(new ReferenceTag("Tag 2", 2));
 
-        cameraPos = new Vector3f(0, 0, 3);
-        cameraRot = new Vector2f(0, 0);
+        cameraRotX = new SmoothFloat(SMOOTH, 0);
+        cameraRotY = new SmoothFloat(SMOOTH, 0);
+
+        cameraPosX = new SmoothFloat(SMOOTH, 0);
+        cameraPosY = new SmoothFloat(SMOOTH, 0);
+        cameraPosZ = new SmoothFloat(SMOOTH, 3);
 
         gizmoOp = Operation.TRANSLATE;
         gizmoMode = Mode.WORLD;
@@ -61,11 +68,18 @@ public final class LocalizationTool extends ViewportTool {
 
     @Override
     protected void drawViewportContent(PGraphics pGraphics) {
+        cameraPosX.step(); cameraPosY.step(); cameraPosZ.step();
+        cameraRotX.step(); cameraRotY.step();
+
         // Custom projection and view matrices because Processing's defaults are pretty bad
         PGraphicsOpenGL g = (PGraphicsOpenGL) pGraphics;
         projection = new Matrix4f().perspective((float) Math.toRadians(80), g.width / (float) g.height, 0.01f, 1000f);
         setPMatrix(g.projection, projection);
-        view = new Matrix4f().translate(cameraPos).rotateY(cameraRot.y).rotateX(cameraRot.x).invert();
+        view = new Matrix4f()
+                .translate(new Vector3f(cameraPosX.get(), cameraPosY.get(), cameraPosZ.get()))
+                .rotateY(cameraRotY.get())
+                .rotateX(cameraRotX.get())
+                .invert();
         setPMatrix(g.modelview, view);
         g.modelviewInv.set(g.modelview);
         g.modelviewInv.invert();
@@ -159,8 +173,12 @@ public final class LocalizationTool extends ViewportTool {
                     Vector3f up = viewRotInv.transformPosition(new Vector3f(0, 1, 0)).normalize();
                     Vector3f right = viewRotInv.transformPosition(new Vector3f(1, 0, 0)).normalize();
 
-                    cameraPos.add(up.mul(deltaY * 0.01f));
-                    cameraPos.add(right.mul(deltaX * -0.01f));
+                    float scaleUp = deltaY * 0.01f;
+                    float scaleRight = deltaX * -0.01f;
+
+                    cameraPosX.set(cameraPosX.getTarget() + up.x * scaleUp + right.x * scaleRight);
+                    cameraPosY.set(cameraPosY.getTarget() + up.y * scaleUp + right.y * scaleRight);
+                    cameraPosZ.set(cameraPosZ.getTarget() + up.z * scaleUp + right.z * scaleRight);
                 }
 
                 if (io.getMouseDown(ImGuiMouseButton.Right)) {
@@ -169,12 +187,17 @@ public final class LocalizationTool extends ViewportTool {
                     float deltaX = io.getMouseDeltaX();
                     float deltaY = io.getMouseDeltaY();
 
-                    cameraRot.add(deltaY * 0.007f, deltaX * 0.007f);
+                    cameraRotX.set(cameraRotX.getTarget() + deltaY * 0.007f);
+                    cameraRotY.set(cameraRotY.getTarget() + deltaX * 0.007f);
                 }
 
                 float scroll = io.getMouseWheel();
                 Vector3f forward = viewRotInv.transformPosition(new Vector3f(0, 0, -1)).normalize();
-                cameraPos.add(forward.mul(scroll * -0.2f));
+
+                float scale = scroll * -0.2f;
+                cameraPosX.set(cameraPosX.getTarget() + forward.x * scale);
+                cameraPosY.set(cameraPosY.getTarget() + forward.y * scale);
+                cameraPosZ.set(cameraPosZ.getTarget() + forward.z * scale);
             }
 
             ImGui.tableNextColumn();
