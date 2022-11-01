ac296a71a649a0e72e7a9c5d726ce9a93d7c5f84
rmheuer <63077980+rmheuer@users.noreply.github.com>
2022-11-01 15:30:27 -0500

    Delete Bert

--- Robot_Template/Bert/src/main/java/com/swrobotics/messenger/client/MessageHandlerFunc.java
+++
@@ -1,9 +0,0 @@
-package com.swrobotics.messenger.client;
-
-import java.io.DataInputStream;
-import java.io.IOException;
-
-@FunctionalInterface
-public interface MessageHandlerFunc {
-    void handle(String type, DataInputStream in) throws IOException;
-}
--- Robot_Template/Bert/src/main/java/com/swrobotics/taskmanager/api/TaskOutputHandler.java
+++
@@ -1,6 +0,0 @@
-package com.swrobotics.taskmanager.api;
-
-@FunctionalInterface
-public interface TaskOutputHandler {
-    void handle(String task, String line);
-}
